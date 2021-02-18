9dfd4a4f4d41db801c48268135d167d12bda6152
Javier-Delunado <fjaviercamacholopez@gmail.com>
2021-02-18 21:58:07 +0100

    glew32 static library. Installer V1.0

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -8,4 +8,8 @@ if /i exist "C:\Windows\SysWOW64\assimp-vc142-mt-x64d.dll" Echo assimp-vc142-mt-
 COPY "%~dp0\Dependencies\DWin64\assimp-vc142-mt-x64d.dll" "C:\Windows\System32"
 if /i exist "C:\Windows\System32\assimp-vc142-mt-x64d.dll" Echo assimp-vc142-mt-x64d.dll copied successfully to System32
 
+powershell -command "Expand-Archive -Force '%~dp0Dependencies.zip' '%~dp0'"
+if exist "%~dp0Dependencies" (echo Dependencies extracted) else (echo "Error extracting dependencies. Please, unzip the Dependencies folder manually")
+
+
 pause
